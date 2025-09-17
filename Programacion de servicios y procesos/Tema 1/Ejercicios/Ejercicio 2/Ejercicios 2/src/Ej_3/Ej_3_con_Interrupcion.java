a82e4869c6c52fb26b1d5adcd262b89c5b16b5ed
davdborja731 <dborjam@iesch.org>
2025-09-17 15:45:00 +0200

    2025-09-17

--- Programacion de servicios y procesos/Tema 1/Ejercicios/Ejercicio 2/Ejercicios 2/src/Ej_3/Ej_3_con_Interrupcion.java
+++ Programacion de servicios y procesos/Tema 1/Ejercicios/Ejercicio 2/Ejercicios 2/src/Ej_3/Ej_3_con_Interrupcion.java
@@ -1,19 +1,16 @@
-public class Ej_3_sin_interrupcion {
+package Ej_3;
+
+public class Ej_3_con_Interrupcion {
     public static void main(String[] args) {
-        // Creo hilo que dice hola
+        // Hilo que imprime "Hola"
         Thread hiloHola = new Thread(() -> {
             try {
-                // Bucle for que hace que se ejecute 15 veces
                 for (int i = 1; i <= 15; i++) {
-                    // Compruebo si el hilo se ha interrumpido si es el
-                    // caso imprimo un mensaje y salgo del for
                     if (Thread.currentThread().isInterrupted()) {
-                        System.out.println("\nHilo 'Hola' interrumpido. Finalizando...");
+                        System.out.println("Hilo 'Hola' Finalizado");
                         break;
                     }
-                    // Si no es el caso imprimo hola
                     System.out.print("Hola ");
-                    // Hago un apausa en el hilo de 2 segundos
                     Thread.sleep(2000);
                 }
             } catch (InterruptedException e) {
@@ -21,32 +18,38 @@ public class Ej_3_sin_interrupcion {
             }
         });
 
-        // Creo el hilo hola
+        // Inicio hilo que imprime mundo!
         Thread hiloMundo = new Thread(() -> {
             try {
-                // Bucle for que hace que se ejecute 15 veces el hilo e imprima mundo!
-                // ademas hago una pausa en el hilo de 2 segundos
                 for (int i = 1; i <= 15; i++) {
                     System.out.println("mundo!");
-                    Thread.sleep(2000); // pausa de 2 segundos
+                    Thread.sleep(2000);
                 }
             } catch (InterruptedException e) {
                 System.out.println("Hilo 'Mundo' interrumpido.");
             }
         });
 
-        // Inicio el hilo que imprime hola
+        // Inicio el hilo hola
         hiloHola.start();
 
-        // Retraso para que el hilo que dice hola tenga ventaja visual sobre el hiloMundo
+        // Retraso para sincronizar salida
         try {
             Thread.sleep(20); // 20 milisegundos
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
 
-        // Lanzamos el hilo 'Mundo'
+        // Inicio el hilo que imprime mundo!
         hiloMundo.start();
+
+        // Espero 5 segundos antes de interrumpir el hilo 'Hola'
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        // Interrumpo el hilo hola
+        hiloHola.interrupt();
     }
 }
-
