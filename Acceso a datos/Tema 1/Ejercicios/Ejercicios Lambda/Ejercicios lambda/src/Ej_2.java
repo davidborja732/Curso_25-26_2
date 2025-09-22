3fccd17a3316c00e37c95dd1db028d1a480590b2
davdborja731 <dborjam@iesch.org>
2025-09-22 11:23:31 +0200

    2025-09-22

--- Acceso a datos/Tema 1/Ejercicios/Ejercicios Lambda/Ejercicios lambda/src/Ej_2.java
+++ Acceso a datos/Tema 1/Ejercicios/Ejercicios Lambda/Ejercicios lambda/src/Ej_2.java
@@ -1,5 +1,11 @@
+import java.util.List;
+
 public class Ej_2 {
     static void main() {
-
+        List<String> listapalabras=List.of("Montaña", "Aventura", "Cielo", "Abeja", "Estrella",
+                "Bosque", "Amanecer", "Fuego", "Guitarra", "Amigo",
+                "Jardín", "Dibujo", "Historia", "Acorde", "Luna");
+        List<String> soloconA=listapalabras.stream().filter(s -> s.toLowerCase().startsWith("a")).toList();
+        soloconA.forEach(System.out::println);
     }
 }
