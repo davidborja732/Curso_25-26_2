a098b7824a4d2892ff0abe2ccc485e76f22052a5
davdborja731 <dborjam@iesch.org>
2025-09-30 23:20:05 +0200

    2025-09-30

--- Programacion multimedia y dispositivos moviles/Proyectos Android/02_Superheroes/app/build.gradle.kts
+++ Programacion multimedia y dispositivos moviles/Proyectos Android/02_Superheroes/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    // Añadimos el plugin parzelize
+    id("kotlin-parcelize")
 }
 
 android {
--- Programacion multimedia y dispositivos moviles/Proyectos Android/02_Superheroes/app/src/main/java/org/iesch/a02_registro_superheroes/RegisterActivity.kt
+++ Programacion multimedia y dispositivos moviles/Proyectos Android/02_Superheroes/app/src/main/java/org/iesch/a02_registro_superheroes/RegisterActivity.kt
@@ -1,17 +1,32 @@
 package org.iesch.a02_registro_superheroes
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.media.Image
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import org.iesch.a02_registro_superheroes.databinding.ActivityRegisterBinding
 import org.iesch.a02_registro_superheroes.detalle.DetalleHeroeActivity
+import org.iesch.a02_registro_superheroes.model.Superheroe
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var  binding: ActivityRegisterBinding
-
+    // Creamos un avariable que va a manejar el resultado de haber hecho la foto
+    private var heroBitmap: Bitmap?=null
+    lateinit var heroImage: ImageView
+    private val getContent=registerForActivityResult(ActivityResultContracts.TakePicturePreview()){
+        //Esto devuelve un objeto bitmap
+        bitmap ->
+        heroBitmap =bitmap
+        heroImage.setImageBitmap(heroBitmap)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,22 +42,33 @@ class RegisterActivity : AppCompatActivity() {
             val superheroName=binding.etHeroName.text.toString()
             val alterEgo=binding.etAlterego.text.toString()
             val bio=binding.etVio.text.toString()
-            val power=binding.rbPower.rating
-            iradetalleHeroe(superheroName,alterEgo,bio,power)
+            val power=binding.rbResultado.rating
+            // me creo un ibjeto superhero y lo envio a la funcion
+            val superheroe= Superheroe(superheroName,alterEgo,bio,power)
+            iradetalleHeroe(superheroe)
+        }
+        heroImage=binding.superheroImage
+        binding.superheroImage.setOnClickListener {
+            openCamera()
         }
     }
+    private fun openCamera(){
+        //abrimos la camara usando getcontent launch
+        getContent.launch(null)
+    }
 }
-
-private fun RegisterActivity.iradetalleHeroe(nombre: String,ego: String,bio: String,power: Float) {
+private fun RegisterActivity.iradetalleHeroe(superheroe: Superheroe) {
     //al pulsar el boton quiero ir a la otra pantalla
     // El intent tiene que tener claro el destino y el origen
-    val intent= Intent(this, DetalleHeroeActivity::class.java)
     // Añado los valores al Intent con la funcion putExtra
-    intent.putExtra("heroname",nombre)
-    intent.putExtra("heroego",ego)
-    intent.putExtra("herobio",bio)
-    intent.putExtra("heropower",power)
-    
+    /*intent.putExtra(DetalleHeroeActivity.HERO_NAME,superheroe.nombre)
+    intent.putExtra(DetalleHeroeActivity.ALTER_EGO,superheroe.alterego)
+    intent.putExtra(DetalleHeroeActivity.BIO,superheroe.bio)
+    intent.putExtra(DetalleHeroeActivity.POWER,superheroe.power)*/
+    val intent= Intent(this, DetalleHeroeActivity::class.java)
+    intent.putExtra(DetalleHeroeActivity.SUPERHERO_KEY,superheroe)
+    //añado un objeto bitmap al intent
+    intent.putExtra(DetalleHeroeActivity.SUPERHERO_FOTO,heroImage.drawable.toBitmap())
     startActivity(intent)
 
 }
