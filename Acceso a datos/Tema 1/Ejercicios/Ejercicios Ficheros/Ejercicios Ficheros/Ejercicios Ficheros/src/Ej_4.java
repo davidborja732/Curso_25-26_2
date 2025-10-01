14d046180d7e938263884572cc3327a8aa45d34b
davdborja731 <dborjam@iesch.org>
2025-10-01 13:13:52 +0200

    2025-10-01

--- Acceso a datos/Tema 1/Ejercicios/Ejercicios Ficheros/Ejercicios Ficheros/Ejercicios Ficheros/src/Ej_4.java
+++ Acceso a datos/Tema 1/Ejercicios/Ejercicios Ficheros/Ejercicios Ficheros/Ejercicios Ficheros/src/Ej_4.java
@@ -4,6 +4,7 @@ public class Ej_4 {
     public static void main(String[] args) {
         File file=new File("Archivos Ejercicios");
         File[] archivostxt=file.listFiles();
+        assert archivostxt != null;
         for (File archivo :archivostxt){
             try (BufferedReader bufferedReader = new BufferedReader(new FileReader(archivo));
                  BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("Archivo_Salida.txt",true))
@@ -13,8 +14,6 @@ public class Ej_4 {
                     bufferedWriter.write(linea+"\n");
                 }
                 bufferedWriter.write("------------------------------"+"\n");
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
