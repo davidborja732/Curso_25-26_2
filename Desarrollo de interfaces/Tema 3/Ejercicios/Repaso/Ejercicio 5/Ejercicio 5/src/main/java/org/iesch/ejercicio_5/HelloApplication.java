eac0fbb2f3e83368930673f037b6779045c1e676
davdborja731 <dborjam@iesch.org>
2026-01-11 15:25:48 +0100

    2026-01-11

--- Desarrollo de interfaces/Tema 3/Ejercicios/Repaso/Ejercicio 5/Ejercicio 5/src/main/java/org/iesch/ejercicio_5/HelloApplication.java
+++ Desarrollo de interfaces/Tema 3/Ejercicios/Repaso/Ejercicio 5/Ejercicio 5/src/main/java/org/iesch/ejercicio_5/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        stage.getIcons().add(new Image("Desarrollo.png"));
+        //stage.getIcons().add(new Image("Desarrollo.png"));
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
