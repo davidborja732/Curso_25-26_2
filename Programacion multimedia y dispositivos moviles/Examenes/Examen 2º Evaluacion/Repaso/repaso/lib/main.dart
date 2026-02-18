18e85492b068d05c95f85838d8304bdc7309e9b2
davdborja731 <dborjam@iesch.org>
2026-02-18 20:50:08 +0100

    2026-02-18

--- Programacion multimedia y dispositivos moviles/Examenes/Examen 2º Evaluacion/Repaso/repaso/lib/main.dart
+++ Programacion multimedia y dispositivos moviles/Examenes/Examen 2º Evaluacion/Repaso/repaso/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        '/':(context) =>PruebaScreen(),
+        '/':(context) =>PruebaScreen(personas: [],),
       },
     );
   }
--- Programacion multimedia y dispositivos moviles/Examenes/Examen 2º Evaluacion/Repaso/repaso/lib/prueba_screen.dart
+++ Programacion multimedia y dispositivos moviles/Examenes/Examen 2º Evaluacion/Repaso/repaso/lib/prueba_screen.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:repaso/personas_response.dart';
 
 class PruebaScreen extends StatelessWidget {
+  final List<PersonasResponse> personas;
 
-  const PruebaScreen({Key? key}) : super(key: key);
-  
+  const PruebaScreen({Key? key, required this.personas}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home'),),
-      body: Center(
-        child: Text('HomeScreen'),
+      appBar: AppBar(title: Text('Personas')),
+      body: ListView.builder(
+        itemCount: personas.length,
+        itemBuilder: (context, index) {
+          final persona = personas[index];
+          return ListTile(
+            leading: CircleAvatar(child: Text(persona.name[0])),
+            title: Text(persona.name),
+            subtitle: Text(persona.email),
+          );
+        },
       ),
     );
   }
